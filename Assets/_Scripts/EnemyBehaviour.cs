2dbe18261fab1d31f8a386afd76e0569beedbd24
FidyBack <abel.cavalcante.ismart@gmail.com>
2022-06-07 13:43:40 -0300

    Mta coisa

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour {
+    private Transform player;
+    private NavMeshAgent nav;
+
+
+    public void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    public void Update() {
+        nav.SetDestination(player.position);
+    }
+
+}
